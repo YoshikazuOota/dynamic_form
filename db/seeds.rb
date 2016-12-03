c79d8685c9ba49bd68ecab7555ccab6a539fa0e4
Yoshikazu Oota <atticatticattic@gmail.com>
2016-12-03 13:17:49 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Item.create!([
+  {name: "文字列入力", typ: 1, presence: false, only_integer: false, format_with: ""},
+  {name: "数字", typ: 2, presence: true, only_integer: true, format_with: ""},
+  {name: "日時", typ: 3, presence: true, only_integer: false, format_with: ""},
+  {name: "アルファベットのみ", typ: 1, presence: true, only_integer: false, format_with: "\\A[a-zA-Z]+\\z"}
+])
